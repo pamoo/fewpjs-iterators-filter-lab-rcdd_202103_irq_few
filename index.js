dc8d57cfe8696dd9ae6f5ea6fc8201b0fd459ccf
Payam Abdullah <payam97abubakr@gmail.com>
2021-05-21 08:27:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (array, string) => {
+  return array.filter(x => x.toLowerCase() === string.toLowerCase())
+}
+
+ const fuzzyMatch = (array, string) => {
+  return array.filter(x => x.slice(0, string.length) === string)
+}
+
+ const matchName = (array, string) => {
+  return array.filter(x => x.name === string)
+}
